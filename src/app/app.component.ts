a9e1ccb0ffb6f73f09832f2778e65248b8c1b6c2
Jusef Belkasemi <jusef@in-fact.com>
2019-01-15 16:15:26 +0000

    implement examples in the coding the smart way animations tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  toState;
+
+  changeState(state){
+    this.toState = state;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { AnimateComponent } from './components/animate/animate.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AnimateComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
